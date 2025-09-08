c03161bee15b3b275b401c51f11fd2dab1d62a6f
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-09-08 22:07:57 +0600

    load words by level and display cards

--- script/index.js
+++ script/index.js
@@ -1,28 +1,48 @@
 const loadLessons = () => {
   fetch("https://openapi.programming-hero.com/api/levels/all") // promise of response
     .then((res) => res.json()) // promise of json data
-    .then((json) => displayLesson(json.data)); 
+    .then((json) => displayLesson(json.data));
+};
+
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data.data));
+};
+
+const displayLevelWord = (words) => {
+  const wordContainer = document.getElementById("word-container");
+//   wordContainer.innerHTML = "";
+
+  words.forEach((word) => {
+    console.log(word);
+    const card = document.createElement("div");
+    card.innerHTML = `
+            <p>Cat</p>
+        `;
+    wordContainer.append(card);
+  });
 };
 
 const displayLesson = (lessons) => {
-    // 1. get the container and empty
-    const levelContainer = document.getElementById("level-container");
-    levelContainer.innerHTML = "";
-    // 2. get into every lessons
-    for(let lesson of lessons){
-        // 3. create element
-        console.log(lesson)
-        const btnDiv = document.createElement("div");
-        btnDiv.innerHTML = `
-              <button class="btn btn-outline btn-primary">
+  // 1. get the container and empty
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+  // 2. get into every lessons
+  for (let lesson of lessons) {
+    // 3. create element
+    console.log(lesson);
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+              <button onclick = "loadLevelWord (${lesson.level_no})" class="btn btn-outline btn-primary">
                 <i class="fa-solid fa-book-open"></i>lesson - ${lesson.level_no}
               </button></
             >
-        `
-        // 4. append into container
+        `;
+    // 4. append into container
 
-        levelContainer.append(btnDiv)
-
-    }
-}
+    levelContainer.append(btnDiv);
+  }
+};
 loadLessons();
