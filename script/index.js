b3044fbcd34e3c24bd46689c3a43532e5ce1a8d2
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-09-09 23:49:52 +0600

    highlight active lesson and integrate daisyUI modal

--- script/index.js
+++ script/index.js
@@ -4,11 +4,22 @@ const loadLessons = () => {
     .then((json) => displayLesson(json.data));
 };
 
+const removeActive = () => {
+  const lessonButtons = document.querySelectorAll(".lesson-btn");
+  // console.log(lessonButtons)
+  lessonButtons.forEach((btn) => btn.classList.remove("btn-active"));
+};
+
 const loadLevelWord = (id) => {
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayLevelWord(data.data));
+    .then((data) => {
+      removeActive(); // remove all active class
+      const clickBtn = document.getElementById(`lesson-btn-${id}`);
+      clickBtn.classList.add("btn-active"); // add active class
+      displayLevelWord(data.data);
+    });
 };
 
 const displayLevelWord = (words) => {
@@ -39,7 +50,7 @@ const displayLevelWord = (words) => {
       word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায় নি"
     }</p>
             <div class="flex justify-between items-center">
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>
@@ -58,10 +69,10 @@ const displayLesson = (lessons) => {
     console.log(lesson);
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-              <button onclick = "loadLevelWord (${lesson.level_no})" class="btn btn-outline btn-primary">
+              <button id="lesson-btn-${lesson.level_no}" onclick = "loadLevelWord (${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn">
                 <i class="fa-solid fa-book-open"></i>lesson - ${lesson.level_no}
-              </button></
-            >
+              </button>
+            
         `;
     // 4. append into container
 
