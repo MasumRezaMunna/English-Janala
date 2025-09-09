6f8a08e8ee99a71dd8d6474835b24d534f7ed291
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-09-09 11:45:56 +0600

    handle errors, empty lessons for better UX

--- script/index.js
+++ script/index.js
@@ -13,24 +13,39 @@ const loadLevelWord = (id) => {
 
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
-    wordContainer.innerHTML = "";
+  wordContainer.innerHTML = "";
+
+  if (words.length == 0) {
+    wordContainer.innerHTML = `<div class="text-center col-span-full">
+      <img class="mx-auto" src="./assets/alert-error.png"/>
+        <p class="text-xl font-medium text-gray-400 rounded-xl py-10 space-y-6 font-bangla">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-4xl font-bangla">নেক্সট Lesson এ যান</h2>
+      </div>`;
+    return;
+  }
 
   words.forEach((word) => {
     console.log(word);
     const card = document.createElement("div");
     card.innerHTML = `
             <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <h2 class="font-bold text-2xl">${
+              word.word ? word.word : "শব্দ পাওয়া যায় নি"
+            }</h2>
             <p class="font-semibold">Meaning / Pronunciation</p>
-            <p class="font-semibold text-2xl font-bangla">${word.meaning}/${word.pronunciation}</p>
+            <p class="font-semibold text-2xl font-bangla">${
+              word.meaning ? word.meaning : "অর্থ পাওয়া যায় নি"
+            }/${
+      word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায় নি"
+    }</p>
             <div class="flex justify-between items-center">
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>
         `;
-        wordContainer.append(card);
-    });
+    wordContainer.append(card);
+  });
 };
 
 const displayLesson = (lessons) => {
